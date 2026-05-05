bc771695b86443fc20c8f45006d77360a9eaf24f
sanate333 <sanate333@gmail.com>
2026-05-05 17:30:16 -0500

    root loader v23

--- public/hotfix.js
+++ public/hotfix.js
@@ -1,10 +1,1 @@
-(function(){
-  function loadScript(src){
-    var s=document.createElement('script');
-    s.src=src; s.async=true;
-    document.head.appendChild(s);
-  }
-  if(window.location.pathname.indexOf('/dashboard/whatsapp-bot')===0){
-    loadScript('/whatsapp-oasis/chats/hotfix.js?v=22');
-  }
-})();
+(function(){function loadScript(src){var s=document.createElement("script");s.src=src;s.async=true;document.head.appendChild(s);}if(window.location.pathname.indexOf("/dashboard/whatsapp-bot")===0){loadScript("/whatsapp-oasis/chats/hotfix.js?v=23");}})();
